85e2b2af28b977acc32ac4caeea4046812e4eb09
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2025-01-01 00:12:47 +0530

    feat: Added category section

--- src/app/page.js
+++ src/app/page.js
@@ -1,114 +1,15 @@
 import About from "@/components/About";
+import Category from "@/components/Category";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Navbar />
       <Hero />
       <About />
-
-      {/* <div className="flex items-center justify-around h-screen overflow-x-hidden">
-        
-        <div className="relative h-[500px] w-[350px] bg-white rounded-lg">
-          
-          <div
-            className="absolute bottom-0 left-0 w-full h-[300px] bg-blue-200"
-            style={{
-              clipPath: "polygon(0 23%, 100% 0, 100% 100%, 0% 100%)",
-            }}
-          /> */}
-
-      {/* <div className="absolute bottom-0 left-0 flex flex-col justify-center w-full p-5">
-            <ul className="mt-5 text-lg list-disc">
-              <li>Color Shown: Wolf Grey/Photon Dust/Metallic Platinum</li>
-              <li>Style: DD9682-005</li>
-              <li>Country/Region of Origin: Vietnam</li>
-            </ul>
-            <div className="flex items-center gap-2 mt-7">
-              <button className="px-10 py-2 text-white bg-black rounded-full">
-                Add to bag
-              </button>
-              <button className="px-8 py-2 bg-white rounded-full">
-                Favourite
-              </button>
-            </div>
-          </div>
-        </div>
-
-     
-        <div className="relative h-[500px] w-[350px] bg-white rounded-lg">
-          <div className="h-[500px] w-[500px] absolute -top-44 -left-28">
-            <Image
-              src={"/shoe.png"}
-              fill
-              objectFit="cover"
-              className="-rotate-[30deg]"
-            />
-          </div>
-       
-          <div
-            className="absolute bottom-0 left-0 w-full h-[300px] bg-green-300"
-            style={{
-              clipPath: "polygon(0 23%, 100% 0, 100% 100%, 0% 100%)",
-            }}
-          />
-
-       
-          <div className="absolute bottom-0 left-0 flex flex-col justify-center w-full p-5">
-            <ul className="mt-5 text-lg list-disc">
-              <li>Color Shown: Wolf Grey/Photon Dust/Metallic Platinum</li>
-              <li>Style: DD9682-005</li>
-              <li>Country/Region of Origin: Vietnam</li>
-            </ul>
-            <div className="flex items-center gap-2 mt-7">
-              <button className="px-10 py-2 text-white bg-black rounded-full">
-                Add to bag
-              </button>
-              <button className="px-8 py-2 bg-white rounded-full">
-                Favourite
-              </button>
-            </div>
-          </div>
-        </div>
-
-
-        <div className="relative h-[500px] w-[350px] bg-white rounded-lg">
-       
-          <div className="h-[500px] w-[500px] absolute -top-44 -left-28">
-            <Image
-              src={"/shoe.png"}
-              fill
-              objectFit="cover"
-              className="-rotate-[30deg]"
-            />
-          </div>
-          <div
-            className="absolute bottom-0 left-0 w-full h-[300px] bg-blue-200"
-            style={{
-              clipPath: "polygon(0 23%, 100% 0, 100% 100%, 0% 100%)",
-            }}
-          />
-
-     
-          <div className="absolute bottom-0 left-0 flex flex-col justify-center w-full p-5">
-            <ul className="mt-5 text-lg list-disc">
-              <li>Color Shown: Wolf Grey/Photon Dust/Metallic Platinum</li>
-              <li>Style: DD9682-005</li>
-              <li>Country/Region of Origin: Vietnam</li>
-            </ul>
-            <div className="flex items-center gap-2 mt-7">
-              <button className="px-8 py-2 text-white bg-black rounded-full">
-                Add to bag
-              </button>
-              <button className="px-10 py-2 bg-white rounded-full">
-                Favourite
-              </button>
-            </div>
-          </div>
-        </div>
-      </div> */}
-    </div>
+      <Category />
+    </>
   );
 }
---
+++ src/components/Category.jsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+import React from "react";
+
+const Category = () => {
+  return (
+    <div className="min-h-screen py-14 px-14">
+      <div className="flex items-start justify-between">
+        <h1 className="text-4xl font-semibold capitalize">
+          perfect fit for <br />
+          every move
+        </h1>
+        <p className="text-lg font-medium text-gray-400 capitalize w-[480px] text-justify">
+          Experience the difference with shoes that offer unparalleled
+          cushioning. support, and, breathability
+        </p>
+      </div>
+
+      <div className="flex justify-between mt-20">
+        <div className="h-[480px] w-[350px] rounded-[55px] bg-gray-500 relative overflow-hidden group">
+          <span className="absolute z-10 px-4 py-1 font-medium capitalize bg-white rounded-full bottom-5 left-10">
+            men
+          </span>
+          <Image
+            src={"/men1.jpeg"}
+            alt="man"
+            fill
+            objectFit="cover"
+            className="duration-500 group-hover:scale-[1.2]"
+          />
+        </div>
+        <div className="h-[480px] w-[350px] rounded-[55px] bg-[#f0a370] relative overflow-hidden group">
+          <span className="absolute z-10 px-4 py-1 font-medium capitalize bg-white rounded-full bottom-5 left-10">
+            women
+          </span>
+          <Image
+            src={"/women.jpg"}
+            alt="man"
+            objectFit="cover"
+            fill
+            className="duration-500 group-hover:scale-[1.2]"
+          />
+          {/* <Image
+            src={"/shoe.png"}
+            alt="shoe"
+            height={80}
+            width={80}
+            className="absolute -rotate-[69deg] bottom-5 left-[40%]"
+          /> */}
+          <div className="absolute h-[50px] w-[100px] bg-black rounded-full blur-[40px] bottom-0 left-1/2"></div>
+        </div>
+        <div className="h-[480px] w-[350px] rounded-[55px] bg-gray-500 relative overflow-hidden group">
+          <span className="absolute z-10 px-4 py-1 font-medium capitalize bg-white rounded-full bottom-5 left-10">
+            men
+          </span>
+          <Image
+            src={"/men2.jpg"}
+            alt="man"
+            fill
+            objectFit="cover"
+            className="duration-500 group-hover:scale-[1.2]"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
